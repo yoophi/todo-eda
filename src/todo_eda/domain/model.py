666d782f57aaedf4d5a256bc40b53ab933a48b08
Pyunghyuk Yoo <yoophi@gmail.com>
2021-08-07 21:45:13 +0900

    ✨ NEW: Todo model

---
+++ src/todo_eda/domain/model.py
@@ -0,0 +1,15 @@
+class Todo:
+    id: str
+    title: str
+    completed: bool
+
+    def __init__(self, id, title, completed):
+        self.id = id
+        self.title = title
+        self.completed = completed
+
+    def to_dict(self):
+        return {
+            key: getattr(self, key)
+            for key in ('id', 'title', 'completed',)
+        }
